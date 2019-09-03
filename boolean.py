976e1f2ada79b86a8e97fa36ca5f6ee4d20cd344
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-03 13:29:28 -0400

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,20 @@
+(2.27) Write a compound Boolean expression that returns True if the value of the variable count is between 1 and 10 inclusive. 
+Run the expression with a value of 1, 5, 9, 10, and 12. Output the value of count and the return value for each.
+
+>>> a= 1-10
+
+>>> b= >10
+
+>>> if a>=b:
+
+          c=1
+
+    else:
+
+          c=0
+
+>>> c
+
+
+
+>>>
