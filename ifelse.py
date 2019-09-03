53024472efd6877e730bcd185e989eb7e056fdc4
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-03 13:40:01 -0400

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,25 @@
+(2.30) Write a nested selection using ifelse that sets the value of a variable gradepoint to 4 if a variable named score is greater than 90, 
+3 if score is between 80 and 89, 2 if score is between 70 and 79, 1 if score is between 60 and 69, and 0 otherwise.
+
+import random
+import math
+ 
+def montePi(value):
+ 
+        d = random.random ()
+ 
+        if d > 90:
+           value = 4
+        
+        if d == 80-90:
+           value == 3
+            
+        if d == 70-80:
+           value == 2
+            
+        if d == 60-70:
+           value == 1
+           
+        if d < 70
+           value == 0
+       
