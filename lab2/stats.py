320de17def239a056437300bc5056144c0899201
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-07 14:32:47 -0400

    Create stats.py

---
+++ lab2/stats.py
@@ -0,0 +1,27 @@
+Create functions to complete the following: 
+2.8 Compute the sum of the first 100 even numbers. Call the function First100EvenSum. 
+>>> acc=0
+
+>>> for x in range(1,101):
+
+        acc = (acc/2) + .5n 
+
+>>> acc
+
+2.9 Compute the sum of the first 50 odd numbers. Call the function First50OddSum. 
+>>> acc=0
+
+>>> for x in range(0,50):
+
+        acc = (acc/2) + .5n 
+
+>>> acc
+
+2.10 Compute the average of the first 100 odd numbers.  Call the function First100Avg.
+>>> acc=0
+
+>>> for x in range(0,100):
+
+        acc = (acc/2) + .5n 
+
+>>> acc
