172c3dd926b59a07438b02d95ac9123b7320aba7
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-07 14:38:24 -0400

    Create drawCircle.py

---
+++ lab1/drawCircle.py
@@ -0,0 +1,20 @@
+>>> from dp import *
+>>> import turtle
+
+>>> t = turtle.Turtle()
+>>> t.up()
+
+>>> t.backward(200)
+
+>>> t.left(90)
+
+>>> t.down()
+
+>>> drawCircle(t,20)
+>>> drawCircle(t,200)
+
+>>> drawCircle(t,20)
+>>> drawCircle(t,200)
+
+>>> drawCircle(t,20)
+>>> drawCircle(t,200)
