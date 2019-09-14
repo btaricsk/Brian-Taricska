1d37eed0963b267198a4819c518ddd174bf8b6bb
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-14 08:40:16 -0400

    Create myCrypto.py

---
+++ lab 3/myCrypto.py
@@ -0,0 +1,3 @@
+Create a file called myCrypto.py. Implement one of the encryption algorithms using these files. 
+Make sure you add comments indicating the type of encryption algorithm used. 
+You should have a method called encrypt and a method called decrypt at the minimum in this file.
