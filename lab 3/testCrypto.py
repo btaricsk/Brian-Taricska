401d25bb1bb80eefb4a1873e5ad9be0e9dcf9c33
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-14 09:19:11 -0400

    Update testCrypto.py

--- lab 3/testCrypto.py
+++ lab 3/testCrypto.py
@@ -1,1 +1,36 @@
 Create a file called testCrypto.py. In testCrypto.py, test (and output the results) of both encrypt and decrypt 3 times.
+
+def substitutionEncrypt(plainText ,key):
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+plainText = plainText.lower ()
+
+cipherText = ""
+
+for ch in plainText:
+
+btciph = alphabet.find(ch)
+
+cipherText = cipherText + key[btcipher]
+
+return cipherText
+
+testKey1 = "zyxwvutsrqponmlkjihgfedcba"
+
+TestKey2 = "wxyzabcdefghijklmnopqrstuv"
+
+TestKey2 = "abcdefghijklmnopqrstuvwxyz"
+
+cipherText = substitutionEncrypt("i went to the store",testKey1)
+
+print(cipherText)
+
+r dvmg gl gsv hgliv
+
+cipherText = substitutionEncrypt("i went to the store",testKey2)
+
+print(cipherText)
+
+m airx xs xli wxsvi
+
