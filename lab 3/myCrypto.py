96fcbb4cb51c5d9c1e824e76559bda079031799f
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-14 08:54:11 -0400

    Update myCrypto.py

--- lab 3/myCrypto.py
+++ lab 3/myCrypto.py
@@ -1,3 +1,32 @@
 Create a file called myCrypto.py. Implement one of the encryption algorithms using these files. 
 Make sure you add comments indicating the type of encryption algorithm used. 
 You should have a method called encrypt and a method called decrypt at the minimum in this file.
+
+
+Import math
+Import random
+
+def scrambledecrypt(randomnum)
+
+evenchar = ""
+oddchar = ""
+charcount = 1
+
+for ch in plainText:  
+      if charCount % 2 == 0:  
+          evenChars = evenChars + ch  
+      else:  
+          oddChars = oddChars + ch  
+    charCount = charCount + 1  
+  cipherText = oddChars + evenChars  
+
+for ch in ciphertext:  
+      if charCount % 2 == 0:  
+          evenChars = evenChars + ch  
+      else:  
+          oddChars = oddChars + ch  
+    charCount = charCount + 1  
+  cipherText = oddChars + evenChars  
+  return   
+      
+     
