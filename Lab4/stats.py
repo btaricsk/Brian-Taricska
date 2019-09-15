2b5213790a6bebf9f8fbb8555632eb47176ffde7
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-15 09:51:37 -0400

    Update stats.py

--- Lab4/stats.py
+++ Lab4/stats.py
@@ -1,1 +1,1 @@
-
+In code, create functions that compute the mean, median, and mode for a list of integers. Use the following list as a test case: [34, 23, 54, 43, 53, 43, 99, 43, 74, 57, 82, 80, 17]. Include the values of mean, median, and mode in README.md.
