3695d8a9f850ede9f813e1cabc7110b3e69778c3
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-15 09:50:54 -0400

    Create  lists.py

---
+++ Lab4/lists.py
@@ -0,0 +1,4 @@
+Create a list by calling the list() function with the following string "The quick brown fox jumps over the lazy dog." Convert all letters to lowercase before calling the list() function.
+Sort the list.
+Create a function called getMostFrequent() that outputs which letter(s) (excluding spaces) occurs the most often in the sentence. Indicate which letter and the in README.md.
+Create another function called uniqueLetters() that creates a list of all unique characters in the string/list. Then it should ouput each unique character in order.
