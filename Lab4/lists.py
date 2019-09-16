e7b7c588b5364ac8da82a7e8bdb04cebbd7d43c6
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-09-16 13:34:30 -0400

    Update lists.py

--- Lab4/lists.py
+++ Lab4/lists.py
@@ -1,4 +1,23 @@
-Create a list by calling the list() function with the following string "The quick brown fox jumps over the lazy dog." Convert all letters to lowercase before calling the list() function.
+Create a list by calling the list() function with the following string "The quick brown fox jumps over the lazy dog." 
+Convert all letters to lowercase before calling the list() function.
 Sort the list.
-Create a function called getMostFrequent() that outputs which letter(s) (excluding spaces) occurs the most often in the sentence. Indicate which letter and the in README.md.
-Create another function called uniqueLetters() that creates a list of all unique characters in the string/list. Then it should ouput each unique character in order.
+Create a function called getMostFrequent() that outputs which letter(s) (excluding spaces) occurs the most often in the sentence. 
+Indicate which letter and the in README.md.
+Create another function called uniqueLetters() that creates a list of all unique characters in the string/list. 
+Then it should ouput each unique character in order.
+
+def mylist(s):
+    alphabetList = 'abcdefghijklmnopqrstuvwxyz'
+    alphabetCount = 0
+    print[ord(char) - 96
+    input = raw_input('Write Text: ')
+    input = input.lower()
+    output = []
+    for character in input:
+        number = ord(character) - 96
+    output.append(number)
+    print "The quick brown fox jumps over the lazy dog." 
+    
+
+
+
