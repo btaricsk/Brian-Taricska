c527081364a5a4dafc46deab3984ef7c7411ea09
btaricsk <54606965+btaricsk@users.noreply.github.com>
2019-10-06 08:12:50 -0400

    Create Lab7.py

---
+++ lab7/Lab7.py
@@ -0,0 +1,8 @@
+Create a file called Lab7.py. In this .py file you will complete the following coding exercises from the book. Please comment your solutions.
+Exercise 8.9 (in the Chapter) - Now that you have seen the details of the railDecrypt function you can make it smarter in two ways:
+You do not need to check cases where the number of rails is greater than the message length divided by two. Can you explain why?
+You only need to check cases where the number of rails evenly divides the total message length. Can you explain why?
+Exercise 8.14 (in the Chapter) - Write a function wordPop that accepts a text and a length N and returns the list of all the words that are N letters long, sorted by their length.
+Exercise 8.18 - Write a regular expression pattern to match all words ending in ing.
+Exercise 8.20 - Write a regular expression pattern to match all words beginning and ending with the letter a.
+When complete, push your Lab files to GitHub. Make sure it pushed successfully and then post the URL to your folder in comments sections of the Lab assignmen
